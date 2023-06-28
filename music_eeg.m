2716cef1aa489dd9020ccde15bdef237a78899ab
Maria Antônia Amado <48838678+AmadoMaria@users.noreply.github.com>
2023-06-28 17:35:41 -0300

    Adicionando PSD

--- music_eeg.m
+++ music_eeg.m
@@ -9,11 +9,9 @@ rest_1 = EEG_Rest(1:1,:);
 aux = permute(EEG_Songs,[2 3 1]);
 
 %Separando músicas no sensor 1
-song1 = aux(1:1,:, 1); %song 1 nota 1
-song2 =  aux(1:1,:, 2); %nota 3
-song11 = aux(1:1,:, 11); %song 1 %nota 5
-
-%song11_sensor2 = aux(2:2,:, 11);
+song1 = aux(2:2,:, 1); %song 1 nota 1
+song2 =  aux(2:2,:, 2); %nota 3
+song11 = aux(2:2,:, 11); %nota 5
 
 %%
 figure(1)
@@ -74,4 +72,28 @@ ylim([0 50])
 ylabel('Frequencia (Hz)')
 xlabel('Time (s)')
 axis xy
+
+%% Ondas alfa
+% Plotando o PSD das três músicas na mesma figura
+
+%Filtrando as ondas alfa
+song1_filtered = eegfilt(song1, sr, 8, 12);
+song2_filtered = eegfilt(song2, sr, 8, 12);
+song11_filtered = eegfilt(song11, sr, 8, 12);
+
+figure(7)
+hold on
+[pxx1,f1]=pwelch(song1_filtered,2*sr,sr/2,[],sr);
+[pxx2,f2]=pwelch(song2_filtered,2*sr,sr/2,[],sr);
+[pxx11,f11]=pwelch(song11_filtered,2*sr,sr/2,[],sr);
+
+plot(f1, pxx1)
+plot(f2, pxx2)
+plot(f11, pxx11)
+title('Power Spectral Density (PSD)')
+set(gca,'XLim',[0 30])
+hold off
+legend('song1','song2', 'song11')
+ylabel('Potência (mV^2/Hz)')
+xlabel('Frequência (Hz)')
 
